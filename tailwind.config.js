c22d6d453a817df76080cbf33d9c23de2ed518bc
Sean Fong <seancfong@gmail.com>
2023-02-06 20:50:53 -0800

    Add award functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,11 +17,15 @@ module.exports = {
         'sunset': '#d8b26e',
         'cloud': '#dad5d2',
         'sea': '#b4c1c5', 
-        'github': '#28a745'
+        'github': '#28a745',
+        'award': '#ff0000'
       },
       boxShadow: {
         'hero': '-3px 3px 30px -5px rgba(216, 178, 110, 0.2)',
         'about': '15px 20px 40px -10px rgba(216, 178, 110, 0.3)',
+      },
+      dropShadow: {
+        'award': '0 0 15px rgba(234,179,8,0.5)'
       },
       animation: {
         'circleload': 'circleload 1.8s ease-in-out',
